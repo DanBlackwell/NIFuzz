626158208afb6c508b40f989d8eae533d8f032ab
Dan Blackwell <dblackwell@viru10.viru10>
2024-01-29 13:13:26 +0000

    set experiment length back to 12 hours

--- run_fuzz_experiment.sh
+++ run_fuzz_experiment.sh
@@ -12,7 +12,7 @@ cd $SCRIPT_DIR
 
 echo "Building QuantiLeakFuzzer fuzzing experiment"
 NAME="quanti_exp"
-OUTPUT_DIR="results/QuantiLeakFuzzer/$(hostname)_"$(date -d "today" +"%Y_%m_%d_%H%M")"
+OUTPUT_DIR="results/QuantiLeakFuzzer/$(hostname)_$(date -d "today" +"%Y_%m_%d_%H%M")"
 
 IMAGE="${NAME}_image"
 docker build -t $IMAGE -f Dockerfile.fuzz .
